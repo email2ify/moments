1bf2855ef9118c8ff96717bbc8609a0b77b49023
email2ify <>
2023-05-26 15:34:52 +0000

    update the navbar homepage

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,8 @@ import axios from "axios";
 axios.defaults.baseURL = "https://moments12.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
